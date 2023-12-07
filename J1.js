c4097e6e0144c8ac140f2dcc695ac3e31c54877d
Saerlock <maximealbert.pro@gmail.com>
2023-12-07 01:35:31 +0100

    J1 - Version corrigée !

--- J1.js
+++ J1.js
@@ -1,6 +1,6 @@
 const { readFileSync } = require('fs');
 
-function readInput(fileName) {
+const readInput = (fileName) => {
 	return readFileSync(fileName, "utf8", (err, data) => {
 		if (err) throw err;
 		return data.toString();
@@ -29,91 +29,88 @@ function getNumberFormated(text) {
     textTab.pop();
 
     return textTab.map((elem) => {
-                const match = elem.match(/([\d]|one|two|three|four|five|six|seven|eight|nine)/g);
+        const matchFirst = elem.match(/([\d]|one|two|three|four|five|six|seven|eight|nine)/g);
+        let first = matchFirst[0];
 
-                let first = match[0];
-                let last = match[match.length - 1];            
+        const matchLast = elem.split('').reverse().join('').match(/([\d]|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)/g);
+        let last = matchLast[0].split('').reverse('').join('');            
 
-                if (!first.match(/[0-9]/)) {
-                    const matchFiltered = elem.replace(first, first.charAt(0) + first + first.charAt(first.length-1)).match(/[\d]|one|two|three|four|five|six|seven|eight|nine/g);
-                    last = matchFiltered[matchFiltered.length - 1];     
+        if (!first.match(/[0-9]/)) {
+            switch(first) {
+                case 'one':
+                    first = '1';
+                    break;
+                case 'two':
+                    first = '2';
+                    break;
+                case 'three':
+                    first = '3'
+                    break;
+                case 'four':
+                    first = '4'
+                    break;
+                case 'five':
+                    first = '5'
+                    break;
+                case 'six':
+                    first = '6'
+                    break;
+                case 'seven':
+                    first = '7'
+                    break;
+                case 'eight':
+                    first = '8'
+                    break;
+                case 'nine':
+                    first = '9'
+                    break;
+                default:
+                    break;
+            }
+        }                   
 
-                    switch(first) {
-                        case 'one':
-                            first = '1';
-                            break;
-                        case 'two':
-                            first = '2';
-                            break;
-                        case 'three':
-                            first = '3'
-                            break;
-                        case 'four':
-                            first = '4'
-                            break;
-                        case 'five':
-                            first = '5'
-                            break;
-                        case 'six':
-                            first = '6'
-                            break;
-                        case 'seven':
-                            first = '7'
-                            break;
-                        case 'eight':
-                            first = '8'
-                            break;
-                        case 'nine':
-                            first = '9'
-                            break;
-                        default:
-                            break;
-                    }
-                }                   
-
-                if (!last.match(/[0-9]/)) {
-                    switch(last) {
-                        case 'one':
-                            last = '1';
-                            break;
-                        case 'two':
-                            last = '2';
-                            break;
-                        case 'three':
-                            last = '3'
-                            break;
-                        case 'four':
-                            last = '4'
-                            break;
-                        case 'five':
-                            last = '5'
-                            break;
-                        case 'six':
-                            last = '6'
-                            break;
-                        case 'seven':
-                            last = '7'
-                            break;
-                        case 'eight':
-                            last = '8'
-                            break;
-                        case 'nine':
-                            last = '9'
-                            break;
-                        default:
-                            break;
-                    }
-                }
-                
-                return parseInt(first + last);
-            })
-            .reduce((x, y) => x + y, 0)
+        if (!last.match(/[0-9]/)) {
+            switch(last) {
+                case 'one':
+                    last = '1';
+                    break;
+                case 'two':
+                    last = '2';
+                    break;
+                case 'three':
+                    last = '3'
+                    break;
+                case 'four':
+                    last = '4'
+                    break;
+                case 'five':
+                    last = '5'
+                    break;
+                case 'six':
+                    last = '6'
+                    break;
+                case 'seven':
+                    last = '7'
+                    break;
+                case 'eight':
+                    last = '8'
+                    break;
+                case 'nine':
+                    last = '9'
+                    break;
+                default:
+                    break;
+            }
+        }
+        
+        return parseInt(first + last);
+    })
+    .reduce((x, y) => x + y, 0)
 }
 
 
 
 // Tests
-
 const txt1 = "1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet\n";
 const txt2 = "two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen\n";
 const txt3 = "twone\n3sevenineight\n"
@@ -121,20 +118,17 @@ const txt4 = "threethreetwothree\n";
 const txt5 = "five\n";
 const txt6 = "sevenine\n";
 
-
 // console.log(`Test 1: ${getNumber(txt1)} is supposed to equal 142`);
 // console.log(`Test 2: ${getNumberFormated(txt2)} is supposed to equal 281`);
 // console.log(`Test 3: ${getNumberFormated(txt3)} is supposed to equal 59`);
 // console.log(`Test 4: ${getNumberFormated(txt4)} is supposed to equal 33`);
 // console.log(`Test 5: ${getNumberFormated(txt5)} is supposed to equal 55`);
 // console.log(`Test 6: ${getNumberFormated(txt6)} is supposed to equal 79`);
-console.log(getNumberFormated('1sevenseven71d\n'));
-
+// console.log(`Test 7: ${getNumberFormated(txt6)} is supposed to equal 79`);
 
 
 
 // Prod
-
 const txtProd = readInput('inputs/day1.txt');
 console.log(`\nProd 1: ${getNumber(txtProd)}`);
 console.log(`Prod 2: ${getNumberFormated(txtProd)}`);
