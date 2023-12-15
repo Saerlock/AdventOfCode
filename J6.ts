5df3b53cb960b8c55bcdca209c41db737f5943f4
Saerlock <maximealbert.pro@gmail.com>
2023-12-15 07:58:04 +0100

    J6 part2, need to rework, can do better, but it works !

--- J6.ts
+++ J6.ts
@@ -11,7 +11,7 @@ function result(input: number, modif: number) {
     return Math.floor(input/2 - modif) * Math.ceil(input/2 + modif);
 }
 
-function waysToBeatRecord(input: string) : number {
+function waysToWin(input: string) : number {
     const inputTable = input.split('\n').map((x) => x.split(' ').map((x) => parseInt(x)).filter((y) => !isNaN(y)));
     inputTable.pop();
 
@@ -37,6 +37,23 @@ function waysToBeatRecord(input: string) : number {
 
 
 // Part 2
+function waysToWinOne(input: string) : number {
+    const inputTable = input.split('\n').map((x) => x.split(' ').map((x) => parseInt(x)).filter((y) => !isNaN(y)).reduce((x, y) => x + y.toString(), '')).map((x) => parseInt(x));
+    inputTable.pop();
+
+    let waysToBeat: number = 0;
+
+    let j = 0;
+        
+    while (result(inputTable[0], j) > inputTable[1]) {
+        waysToBeat++;
+        j++;
+    }
+
+    inputTable[0]%2 === 0 ? waysToBeat = waysToBeat * 2 - 1 : waysToBeat *= 2
+
+    return waysToBeat;
+}
 
 
 
@@ -44,20 +61,20 @@ function waysToBeatRecord(input: string) : number {
 // Tests
 const txtTest: string = 'Time:      7  15   30\nDistance:  9  40  200\n';
 // console.log(txtTest);
-// waysToBeatRecord(txtTest);
+// waysToWinOne(txtTest);
 let start: number;
 
 start = Date.now();
-console.log(`Test 1: ${waysToBeatRecord(txtTest)} should be equal to 288 | ${(Date.now() - start)}ms\n`);
+console.log(`Test 1: ${waysToWin(txtTest)} should be equal to 288 | ${(Date.now() - start)}ms\n`);
 
-// start = Date.now();
-// console.log(`Test 2: ${(txtTest)} should be equal to  | ${(Date.now() - start)}ms\n`);
+start = Date.now();
+console.log(`Test 2: ${waysToWinOne(txtTest)} should be equal to 71503 | ${(Date.now() - start)}ms\n`);
 
 // // Prod
 const txtProd = readInput('inputs/day6.txt');
 
 start = Date.now();
-console.log(`Prod 1: ${waysToBeatRecord(txtProd)} | ${(Date.now() - start)}ms\n`);
+console.log(`Prod 1: ${waysToWin(txtProd)} | ${(Date.now() - start)}ms\n`);
 
-// start = Date.now();
-// console.log(`Prod 2: ${(txtProd)} | ${(Date.now() - start)}ms\n`);
+start = Date.now();
+console.log(`Prod 2: ${waysToWinOne(txtProd)} | ${(Date.now() - start)}ms\n`);
