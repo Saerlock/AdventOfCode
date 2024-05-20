65c2f09a8a981235b27b259a923f21a2cc7f4214
Saerlock <maximealbert.pro@gmail.com>
2024-05-20 17:38:36 +0200

    J9 part2

--- J9.ts
+++ J9.ts
@@ -6,13 +6,15 @@ const readInput = (fileName) => {
 
 
 
-// Part 1
-
+// Functions
 function allSame(line: number[]): boolean {
 	if(line.length === line.filter((x) => x === line[0]).length) return true;
 	return false;
 }
 
+
+
+// Part 1
 function predictNextValue(line: string[]) : number {
 	let nextValue = 0;
 	let currentLine = line.map(x => parseInt(x));
@@ -37,8 +39,34 @@ function sumOfNextValues(lines: string) : number {
 		.reduce((x, y) => x + y);
 }
 
-// Part 2
 
+
+// Part 2
+function predictExtrapolatedValue(line: string[]) : number {
+	let currentLine = line.map(x => parseInt(x));
+	let extrValue = currentLine[0];
+	let cptLine = 0;
+
+	while(!allSame(currentLine)) {
+		const newLine: number[] = [];
+		for (let i = 0; i < currentLine.length - 1; i++) {
+			newLine.push(currentLine[i+1] - currentLine[i])
+		}
+
+		currentLine = newLine;
+		cptLine++;
+
+		cptLine %2 === 0 ? extrValue += currentLine[0] : extrValue -= currentLine[0];
+	}
+
+	return extrValue;
+}
+
+function sumOfExtrapolatedValues(lines: string) : number {
+	return lines.trim().split('\n')
+		.map((line) => predictExtrapolatedValue(line.split(' ')))
+		.reduce((x, y) => x + y);
+}
 
 
 // Tests
@@ -48,8 +76,8 @@ let start: number;
 start = Date.now();
 console.log(`Test 1: ${sumOfNextValues(txtTest1)} should be equal to 114 | ${(Date.now() - start)}ms\n`);
 
-// start = Date.now();
-// console.log(`Test 2: ${b(txtTest1)} should be equal to  | ${(Date.now() - start)}ms\n`);
+start = Date.now();
+console.log(`Test 2: ${sumOfExtrapolatedValues(txtTest1)} should be equal to 2 | ${(Date.now() - start)}ms\n`);
 
 // Prod
 const txtProd = readInput('inputs/day9.txt');
@@ -57,5 +85,5 @@ const txtProd = readInput('inputs/day9.txt');
 start = Date.now();
 console.log(`Prod 1: ${sumOfNextValues(txtProd)} | ${(Date.now() - start)}ms\n`);
 
-// start = Date.now();
-// console.log(`Prod 2: ${b(txtProd)} | ${(Date.now() - start)}ms\n`);
+start = Date.now();
+console.log(`Prod 2: ${sumOfExtrapolatedValues(txtProd)} | ${(Date.now() - start)}ms\n`);
